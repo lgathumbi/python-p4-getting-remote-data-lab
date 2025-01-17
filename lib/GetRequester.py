5e956cef3899e3acb15a16dd9536db321d8f52ab
lorna gathumbi <nyawiralornah@gmail.com>
2025-01-17 11:19:43 +0300

    getting remote data

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -8,6 +8,11 @@ class GetRequester:
 
     def get_response_body(self):
         pass
+        response = requests.get(self.url)
+        response.raise_for_status()
+        return response.content
 
     def load_json(self):
         pass
+        response_body = self.get_response_body()
+        return json.loads(response_body) 
